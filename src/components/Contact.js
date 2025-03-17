d35e6042aa3bd648a59ce23394a11e77d2f375a3
hariharanp05 <128158383+hariharanp05@users.noreply.github.com>
2025-03-17 13:13:20 +0530

    Update Contact.js
    change style of footer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,13 +10,13 @@ const Contact = () => {
         <Button href="https://www.linkedin.com/in/hariharan-p-4800462b6">hariharan_p|LinkedIn</Button></div><br/>
         <div id="foot">
         <div className='text-white d-md-flex justify-content-evenly' id='footer'>
-            <a href="#home" className=' link-dark text-decoration-none'>Home</a>
-            <a href="#about" className='link-dark text-decoration-none'>About</a>
-            <a href="#skill" className='link-dark text-decoration-none'>Skills</a>
-            <a href="#certificate" className='link-dark text-decoration-none'>Certificate</a>
-            <a href="#intern" className='link-dark text-decoration-none'>Internship</a>
-            <a href="#project" className='link-dark text-decoration-none'>Projects</a>
-            <a href="#contact" className='link-dark text-decoration-none'>Contact</a>
+            <a href="#home" className=' link-light text-decoration-none'>Home</a>
+            <a href="#about" className='link-light text-decoration-none'>About</a>
+            <a href="#skill" className='link-light text-decoration-none'>Skills</a>
+            <a href="#certificate" className='link-light text-decoration-none'>Certificate</a>
+            <a href="#intern" className='link-light text-decoration-none'>Internship</a>
+            <a href="#project" className='link-light text-decoration-none'>Projects</a>
+            <a href="#contact" className='link-light text-decoration-none'>Contact</a>
         </div>
         <div className='py-4'>
         <p className='text-center '>@2024 Hariharan Panneerselvam<br/>Thank you for visiting my Page</p>
