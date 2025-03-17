72801a61c2b48aa18da3609830b61736a3b8b964
hariharanp05 <128158383+hariharanp05@users.noreply.github.com>
2025-03-17 13:04:29 +0530

    Update Hero.js
    change insta link and linkedin link

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,11 +16,11 @@ const Hero = () => {
                 <p className="h1">hello..&#x1F44B;Welcome..&#x1F64F;<br /><i>I'm Hariharan </i> </p>
                 <p>A Passinate Web Developer</p>
                 <div id="social">
-                <a href="https://www.linkedin.com/in/hariharan-panneerselvam-4800462b6"><i class="bi bi-linkedin"></i></a>
-                <a href="https://www.instagram.com/hariharan_panneerselvam?igsh=MTgOdjRmODMyc2J4dg=="><i class="bi bi-instagram"></i></a>
+                <a href="https://www.linkedin.com/in/hariharan-p-4800462b6"><i class="bi bi-linkedin"></i></a>
+                <a href="https://www.instagram.com/hariharan__72?igsh=MTgOdjRmODMyc2J4dg=="><i class="bi bi-instagram"></i></a>
                 <a href="https://wa.me/8220539979"><i class="bi bi-whatsapp"></i></a>
                 </div><br />
-                 <Button href={resume} download='resume'>Resume <i class="bi bi-download"></i></Button>
+                 <Button href={resume} download='HariResume070503'>Resume <i class="bi bi-download"></i></Button>
                 </div>
                 </Col>
             </Row>
